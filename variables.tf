1e11fe8697e6dc3c1ac06831fb4ac2ca8c923040
tests <117904277+ingress-controller@users.noreply.github.com>
2023-03-30 17:47:38 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "namespace" {
+    type = string
+    default = "web"
+}
+
+variable "app_name" {
+    type = string
+    default = "webapp"
+}
+
+variable "site_image" {
+    type = string
+    default = "nginx:alpine"
+}
+
+variable "container_name" {
+    type = string
+    default = "server"
+}
+
+variable "container_port" {
+    type = string
+    default = 80
+}
+
+variable "secret_name" {
+    type = string
+    default = "tls-secret"
+}
+
+variable "session_affinity" {
+    type = string
+    default = "ClientIP"
+}
+
+variable "target_port" {
+    type = string
+    default = 80
+}
+
+variable "port_type" {
+    type = string
+    default = "NodePort"
+}
